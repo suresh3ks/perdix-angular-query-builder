1dc84d9a4587e6df13e97b0bafed1d61bbfa8c40
pkempenaers <pieter.kempenaers@gmail.com>
2016-03-09 13:17:28 +0100

    Added documentation and made ready for gh-pages

--- query-builder-controller.js
+++ query-builder-controller.js
@@ -1,107 +1,114 @@
-﻿var app = angular.module('app', ['ngSanitize', 'queryBuilder']);
+﻿var app = angular.module('app', ['ngSanitize', 'queryBuilder', 'hljs', 'ui.bootstrap']);
+app.config(['hljsServiceProvider', function(hljsServiceProvider) {
+	hljsServiceProvider.setOptions({
+		// replace tab with 4 spaces
+		tabReplace: '  '
+	});
+}]);
 app.controller('QueryBuilderCtrl', ['$scope', '$templateCache', 'queryService', function($scope, $templateCache, queryService) {
-	$templateCache.put('test',
-		'<div ng-dropdown-multiselect options="rule.field.options" selected-model="rule.data"></div>'
-	);
-	$templateCache.put('singleTest', '<div ng-dropdown-multiselect options="' +
-		'rule.field.options" selected-model="rule.data" extra-settings="' +
-		'rule.comparator.multiselectSetting"></div>');
-
-	$scope.fields = [
+	$scope.fields1 = [
 		{
 			id: 1,
-			name: 'IsAwsome',
-			options: [
-				{ name: 'wow', id: 1, label: 'wow' },
-				{ name: 'yes', id: 2, label: 'yes' }
-			]
-		},
-		{ name: 'Lastname', id: 2 },
-		{
-			name: 'Birthdate', id: 3,
-			disabledComparators: [
-					1
-			]
+			name: 'Gender'
 		},
 		{
-			name: 'City', id: 4,
-			options: [
-				{ label: 'paris', name: 'paris', id: 1 },
-				{ label: 'london', name: 'london', id: 2 },
-				{ label: 'brussels', name: 'brussels', id: 3 }
-			]
+			id: 2,
+			name: 'Age'
 		},
 		{
-			name: 'Country', id: 5
-		},
-		{ name: 'FirstName', id: 6 }
+			name: 'Favorite city', id: 4
+		}
 	];
 
-	$scope.example1model = []; $scope.example1data = $scope.fields[0].options;
-
-	$scope.comparators = [
+	$scope.comparators1 = [
 		{ id: 1, name: 'equal to', value: '==' },
 		{ id: 2, name: 'not equal to', value: '!=' },
 		{ id: 3, name: 'smaller than', value: '<' },
 		{ id: 4, name: 'smaller than or equal to', value: '<=' },
 		{ id: 5, name: 'greater than', value: '>' },
-		{ id: 6, name: 'greater than or equal to', value: '>=' },
-		{ id: 7, name: 'in', value: '->', dataTemplate: 'test', defaultData: [] },
-		{
-			id: 8,
-			name: 'equal to select',
-			value: '-<',
-			dataTemplate: 'singleTest',
-			defaultData: {},
-			multiselectSetting: {
-				enableSearch: true,
-				smartButtonMaxItems: 1,
-				smartButtonTextConverter: function(itemText) {
-					return itemText;
-				},
-				showCheckAll: false,
-				showUncheckAll: false,
-				selectionLimit: 1,
-				externalIdProp: ''
-			}
-		}
-	];
-
-	$scope.operators = [
-						{ name: 'AND', value: '&&' },
-						{ name: 'OR', value: '||' }
+		{ id: 6, name: 'greater than or equal to', value: 'equal to or greater' }
 	];
 
 	$scope.operators1 = [
-		{ name: 'AND', value: '&&' }
-	]
+		{ name: 'AND', value: '&&' },
+		{ name: 'OR', value: '||' }
+	];
 
-	var data = {
-		"group":
-		{ "operator": $scope.operators[0], "rules": [] }
+	$scope.json1 = null;
+
+	$scope.filter1 = {
+		group: {
+			operator: $scope.operators1[0], rules: []
+		}
 	};
 
-	$scope.json = null;
-
-	$scope.filter = data;
 	$scope.settings1 = {
 		nesting: false,
 		addIconClass: 'glyphicon glyphicon-plus',
 		removeIconClass: 'glyphicon glyphicon-minus',
 		addButtonClass: 'btn btn-sm btn-success',
-		removeButtonClass: 'btn btn-sm btn-danger',
-		separateLinesWithOperator: true,
-		bootstrapPanelsEnabled: false
+		removeButtonClass: 'btn btn-sm btn-danger'
 	};
 
-	$scope.$watch('filter', function(newValue) {
+	$scope.$watch('filter1', function(newValue) {
 		$scope.json = JSON.stringify(newValue, null, 2);
 		$scope.outputReadable = queryService.asReadable(newValue.group);
 		$scope.outputString = queryService.asString(newValue.group);
 	}, true);
 
+	$scope.fields2 = [
+		{
+			id: 1,
+			name: 'Gender',
+			options: [
+				{ name: 'male', id: 1},
+				{ name: 'female', id: 2}
+			],
+			disabledComparators: [
+				3, 4, 5, 6
+			]
+		},
+		{
+			id: 2,
+			name: 'Age'
+		},
+		{
+			name: 'Favorite city', id: 3,
+			options: [
+				{ name: 'paris', id: 1 },
+				{ name: 'london', id: 2 },
+				{ name: 'brussels', id: 3 }
+			],
+			disabledComparators: [
+				3, 4, 5, 6
+			]
+		}
+	];
+
+	$scope.comparators2 = [
+		{ id: 1, name: 'equal to', value: '=='},
+		{ id: 2, name: 'not equal to', value: '!=' },
+		{ id: 3, name: 'smaller than', value: '<' },
+		{ id: 4, name: 'smaller than or equal to', value: '<=' },
+		{ id: 5, name: 'greater than', value: '>' },
+		{ id: 6, name: 'greater than or equal to', value: '>=' },
+	];
+
+	$scope.operators2 = [
+		{ name: 'AND', value: '&&' },
+		{ name: 'OR', value: '||' }
+	];
+
+	$scope.settings2 = {
+		nesting: true,
+		addIconClass: 'glyphicon glyphicon-plus',
+		removeIconClass: 'glyphicon glyphicon-minus',
+		addButtonClass: 'btn btn-sm btn-success',
+		removeButtonClass: 'btn btn-sm btn-danger'
+	}
 
 	$scope.filter2 = {};
+	$scope.queryAsString2 = '((2=="23"||3!="2")&&1=="1")';
 	$scope.json2 = null;
 
 	$scope.$watch('filter2', function(newValue) {
@@ -109,8 +116,49 @@ app.controller('QueryBuilderCtrl', ['$scope', '$templateCache', 'queryService', 
 		$scope.output2Readable = queryService.asReadable(newValue.group);
 		$scope.output2String = queryService.asString(newValue.group);
 	}, true);
-	//$scope.queryAsString = '1=="1"&&(2=="3"||5!="Belgium")&&4->"London,Paris"';
-	$scope.queryAsString = '(1=="1"&&(2=="Kempenaers"||5!="Belgium")&&4-<"1")';
-	//$scope.queryAsString = '(1=="1")&&(2=="3")';
-	//$scope.queryAsString = '((1=="1")&&(2=="3"))'
+
+	$scope.fields3 = [
+		{
+			name: 'Favorite city', id: 1,
+			options: [
+				{ label: 'paris', name: 'paris', id: 1 },
+				{ label: 'london', name: 'london', id: 2 },
+				{ label: 'brussels', name: 'brussels', id: 3 }
+			]
+		}
+	];
+
+	$templateCache.put('inTemplate',
+		'<div ng-dropdown-multiselect options="rule.field.options" selected-model="rule.data"></div>'
+	);
+
+	$scope.comparators3 = [
+		{ id: 1, name: 'in', value: '->', dataTemplate: 'inTemplate', defaultData: [] },
+		{ id: 2, name: 'equal to', value: '==' }
+	];
+
+	$scope.operators3 = [
+		{ name: 'AND', value: '&&' }
+	];
+
+	$scope.settings3 = {
+		nesting: false,
+		addIconClass: 'glyphicon glyphicon-plus',
+		removeIconClass: 'glyphicon glyphicon-minus',
+		addButtonClass: 'btn btn-sm btn-success',
+		removeButtonClass: 'btn btn-sm btn-danger'
+	}
+
+	$scope.filter3 = {
+		group: {
+			operator: $scope.operators3[0], rules: []
+		}
+	};
+	$scope.json3 = null;
+
+	$scope.$watch('filter3', function(newValue) {
+		$scope.json2 = JSON.stringify(newValue, null, 2);
+		$scope.output2Readable = queryService.asReadable(newValue.group);
+		$scope.output2String = queryService.asString(newValue.group);
+	}, true);
 }]);
